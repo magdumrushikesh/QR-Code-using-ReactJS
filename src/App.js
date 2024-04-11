24f9a754ae887f47670d9cbd84c53368f899737c
rushikesh4525 <rushikesh4525@gmail.com>
2024-04-11 14:57:11 +0530

    v1.1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+  
+  function generateQR() {
+    let imgBox = document.getElementById("imgBox");
+    let qrImage = document.getElementById("qrImage");
+    let qrText = document.getElementById("qrText");
+
+   
+      qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+      imgBox.classList.add("show-img");
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <p>Enter your text or URL</p>
+        <input type="text" placeholder="Text or URL" id="qrText" onChange={generateQR }   />
+        {/* <input type="text" placeholder="Text or URL" id="qrText1"  onChange={(e) => console.log(e.target.value)}  /> */}
+        
+
+
+        <div id="imgBox">
+          <img src="" id="qrImage" alt="" />
+        </div>
+       
+
+      </div>
+    </>
   );
 }
 
